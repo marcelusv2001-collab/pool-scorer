87d9f97119d8f71f12baff1df6b14b1a25630166
MVP-TV <marcelusv2001@gmail.com>
2026-02-11 13:18:15 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const ASSETS = [
   './8ball.html',
   './9ball.html',
   './8ball-18.html', // Added your new 18-point app
-  './8ball-10 PT.html',
+  './8ball-30.html',
   './manifest.json',
   './sw.js',         // Cache the service worker itself
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
@@ -42,4 +42,5 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+
 
